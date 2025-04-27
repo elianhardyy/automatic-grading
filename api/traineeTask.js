ec0d5bbcfb686c14c6ab4c8fa001d01903a274d7
Elian Hardiawan <elianhardiawan00@gmail.com>
2025-04-27 11:27:52 +0700

    [F0006][FIX] fix trainee

--- api/traineeTask.js
+++ api/traineeTask.js
@@ -1,8 +1,10 @@
 import { api } from ".";
+import { buildQueryParams } from "../utils/queryParam";
 
-    export const traineeTaskAPI = {
-    getAllTraineeTasksByBatchTaskId: (filter) =>
-        api.get(
-            `/trainee-task?page=${filter.page}&size=${filter.size}&sortBy=${filter.sortBy}&direction=${filter.direction}&batchTaskId=${filter.batchTaskId}`
-        )
+export const traineeTaskAPI = {
+  getAllTraineeTasksByBatchTaskId: (batchTaskId, filter) =>
+    api.get(
+      `/trainee-task/batch-task/${batchTaskId}${buildQueryParams(filter)}`
+    ),
+  createTraineTask: (data) => api.post(`/trainee-task`, data),
 };
